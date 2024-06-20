7d6c9e1fa8f2168186b86864b39eabd13e2fd3a2
vksssdks <2820142.cse@pietgroup.co.in>
2024-06-20 20:03:26 +0530

    checking deployments

--- cmd/main.go
+++ cmd/main.go
@@ -14,8 +14,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 	response := Response{Message: "Hello World!"}
 	json.NewEncoder(w).Encode(response)
 }
+func checkHandler(w http.ResponseWriter, r *http.Request){
+	response := Response{Message: "cheking World!"}
+	json.NewEncoder(w).Encode(response)
+}
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/check", checkHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
